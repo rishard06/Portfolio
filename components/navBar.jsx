cd7749e13db6096bfb75eb3530bf873841dca3f8
richard06 <richardmacabus06@gmail.com>
2025-04-12 23:34:53 +0800

    change the animation when hovering in navigation link

--- components/navBar.jsx
+++ components/navBar.jsx
@@ -2,30 +2,61 @@
 
 import React, { memo, useRef } from "react";
 import Link from "next/link.js";
-import { navValues } from "@/libs/linksObjects.js";
+import { spring, motion } from "motion/react";
+import { navValues } from "@/lib/linksObjects.js";
 import CursorEffect from "@/components/cursorEffect.jsx";
 
 function NavBar() {
   const navRef = useRef(null);
 
   return (
-    <div
-      className="flex justify-center items-center p-4 text-white"
-    >
-      <nav 
+    <div className="flex justify-center items-center p-4 text-white">
+      <motion.nav
+        className="fixed z-50 top-2 overflow-hidden whitespace-nowrap flex flex-row gap-6 px-8 py-2 backdrop-blur-lg rounded-full shadow-lg shadow-black/50"
         ref={navRef}
-        className="flex flex-row gap-6 px-8 rounded-full shadow-lg shadow-primary/50 bg-[#023047]"
       >
         {navValues.map((item) => (
-          <Link
+          <motion.div
+            initial="initial"
+            whileHover="hovered"
+            transition="transitions"
+            className="relative flex h-12 w-auto rounded-full px-3 font-semibold"
             key={item.name}
-            href={item.link}
-            className="flex h-14 w-14 gap-6 justify-center items-center"
           >
-            {item.name}
-          </Link>
+            <motion.div
+              className="pointer-events-none"
+              variants={{
+                initial: { y: 0 },
+                hovered: { y: "-100%" },
+                transition: { type: spring, stiffness: 300, damping: 20 },
+              }}
+            >
+              <Link
+                className="w-full h-full flex items-center"
+                href={item.link}
+              >
+                {item.name}
+              </Link>
+            </motion.div>
+
+            <motion.div
+              className="absolute inset-0 h-12 w-auto px-3 font-semibold"
+              variants={{
+                initial: { y: "100%" },
+                hovered: { y: 0 },
+                transitions: { type: spring, stiffness: 300, damping: 20 },
+              }}
+            >
+              <Link
+                className="w-full h-full flex items-center"
+                href={item.link}
+              >
+                {item.name}
+              </Link>
+            </motion.div>
+          </motion.div>
         ))}
-      </nav>
+      </motion.nav>
 
       <CursorEffect navRef={navRef} />
     </div>
